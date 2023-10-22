492c188c2a1aec2ff7b68bfd3ab1759c77082796
nicky <nickylds1958@gmail.com>
2023-10-22 15:52:21 -0600

    s

--- weather/src/componentes/search/Search.jsx
+++ weather/src/componentes/search/Search.jsx
@@ -1,6 +1,23 @@
  
  const Search = () => {
-return (
+    const getData = async (url, setState) => {
+      const res = await fetch(url);
+      const datos = await res.json();
+
+      setState(datos);
+    };
+
+    //// aca esta el use efect
+    useEffect(() => {
+      getData(
+        `https://api.openweathermap.org/data/2.5/weather?q=${country}&appid=2f9b41a511d1351d341bc7bd79cd2e13&units=metric`,
+        setData
+      );
+    }, [country]);
+
+
+    return (
+
     <>
     </>
 )
